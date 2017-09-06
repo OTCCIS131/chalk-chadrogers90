b1da3c994247dc1957cb89fb2a2639b4833b81c4
Chad Rogers <cr0537724@otc.edu>
2017-09-06 13:20:17 -0500

    birthday

---
+++ happybirthday.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const log = console.log;
+ 
+// Combine styled and normal strings 
+log(chalk.green('Who ADAM?'));
+log(chalk.yellow('Yeah its his birthday'));
+log(chalk.white('Alright yeah here we go now'));
+log(chalk.cyan('Happy birthday to you'));
+log(chalk.blue('Yo uh uh'));
+log(chalk.yellow('happy birthday to you'));
+log(chalk.red('happy birthday dear ADAM'));
+log(chalk.cyan('hold up... shouldnt we get extra credit for being such great rappers?'));
+log(chalk.red('Yeah we should!'));
+log(chalk.blue('happy birthday to you'));
+log(chalk.green('Ahh yeah'));
+log(chalk.magenta('Now thats how you sing happy birthday!'));
